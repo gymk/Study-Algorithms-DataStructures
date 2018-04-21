90b68e5ed2dc0e37afec10cf2e9eba3c6deae65f
GYMK <gymk.tech@gmail.com>
2018-04-21 11:45:46 +0900

    Add traversals

--- ternary_trie/ternary_trie.cpp
+++ ternary_trie/ternary_trie.cpp
@@ -4,10 +4,9 @@
 
 using namespace std;
 
-#define ALPHABET_SIZE           254
+#define ALPHABET_SIZE           26
 #define INVALID_ALPHABET_INDEX  (ALPHABET_SIZE+1)
 #define ALPHABET_TERMINATOR     INVALID_ALPHABET_INDEX
-
 
 template <typename ALPHABET_TYPE, typename DATA_TYPE_PTR>
 struct tagTTrieNode
@@ -25,12 +24,13 @@ struct tagTTrieNode
 	}
 };
 
-
 template <typename ALPHABET_TYPE, typename DATA_TYPE_PTR>
 class CTTrie
 {
 	typedef struct tagTTrieNode<ALPHABET_TYPE, DATA_TYPE_PTR> TTRIE_NODE;
 	typedef struct tagTTrieNode<ALPHABET_TYPE, DATA_TYPE_PTR> * TTRIE_NODE_PTR;
+	typedef void (*TrieVisitFunc) (DATA_TYPE_PTR pvData);
+
 public:
 	CTTrie(ALPHABET_TYPE alpbhabetTerminator)
 	{
@@ -154,6 +154,63 @@ public:
 		return nullptr;
 	}
 
+	void InOrderTraverse(TrieVisitFunc pfnVistiFunc)
+	{
+		MESAURE_FUNC_EXECUTION_TIME;
+		cout << "{{{ In Order Start" << endl;
+		DoInOrderTraverse(&m_stRoot, pfnVistiFunc);
+		cout << "}} In Order End" << endl;
+	}
+
+	void PreOrderTraverse(TrieVisitFunc pfnVistiFunc)
+	{
+		MESAURE_FUNC_EXECUTION_TIME;
+		cout << "{{{ Pre Order Start" << endl;
+		DoPreOrderTraverse(&m_stRoot, pfnVistiFunc);
+		cout << "{{{ Pre Order End" << endl;
+	}
+
+	void PostOrderTraverse(TrieVisitFunc pfnVistiFunc)
+	{
+		MESAURE_FUNC_EXECUTION_TIME;
+		cout << "{{{ Post Order Start" << endl;
+		DoPostOrderTraverse(&m_stRoot, pfnVistiFunc);
+		cout << "{{{ Post Order End" << endl;
+	}
+
+private:
+	void DoInOrderTraverse(TTRIE_NODE_PTR pstRoot, TrieVisitFunc & pvFunc)
+	{
+		if(nullptr == pstRoot)
+			return;
+		DoInOrderTraverse(pstRoot->m_pLeft, pvFunc);
+		if (pstRoot->m_split_char == m_AlpbhabetTerminator)
+			pvFunc(pstRoot->m_Data);
+		DoInOrderTraverse(pstRoot->m_pMiddle, pvFunc);
+		DoInOrderTraverse(pstRoot->m_pRight, pvFunc);
+	}
+	
+	void DoPreOrderTraverse(TTRIE_NODE_PTR pstRoot, TrieVisitFunc & pvFunc)
+	{
+		if(nullptr == pstRoot)
+			return;
+		DoPreOrderTraverse(pstRoot->m_pMiddle, pvFunc);
+		DoPreOrderTraverse(pstRoot->m_pLeft, pvFunc);
+		DoPreOrderTraverse(pstRoot->m_pRight, pvFunc);
+		if (pstRoot->m_split_char == m_AlpbhabetTerminator)
+			pvFunc(pstRoot->m_Data);
+	}
+	
+	void DoPostOrderTraverse(TTRIE_NODE_PTR pstRoot, TrieVisitFunc & pvFunc)
+	{
+		if(nullptr == pstRoot)
+			return;
+		DoPostOrderTraverse(pstRoot->m_pLeft, pvFunc);
+		DoPostOrderTraverse(pstRoot->m_pRight, pvFunc);
+		if (pstRoot->m_split_char == m_AlpbhabetTerminator)
+			pvFunc(pstRoot->m_Data);
+		DoPostOrderTraverse(pstRoot->m_pMiddle, pvFunc);
+	}
 private:
 	void DeleteTree(void)
 	{
@@ -185,13 +242,18 @@ private:
     unsigned int    m_ui32TreeDepth;
 };
 
-void PrintCharsAsInt(unsigned char * pui8List, int size)
+void PrintCharsAsInt(char * pui8List, int size)
 {
 	for (int i = 0; i < size; i++)
 	{
 		cout << (unsigned int)(pui8List[i]) << ' ';
 	}
 	cout << endl;
+}
+
+void DataPrintFunc(char * pvData)
+{
+	cout << pvData << endl;
 }
 
 int main(int argc, char * argv[])
@@ -205,18 +267,18 @@ int main(int argc, char * argv[])
     int seed = atoi(argv[1]);
     int depth = atoi(argv[2]);
     int entries = atoi(argv[3]);
-    unsigned char   aui8Alphabets[ALPHABET_SIZE];
-	unsigned char ** ppTestStrList;
-    unsigned char   * pui8Tmp;
+    char   aui8Alphabets[ALPHABET_SIZE];
+	char ** ppTestStrList;
+    char   * pui8Tmp;
     int     i, j;
-	CTTrie<unsigned char, unsigned char*>  ternary_trie(ALPHABET_TERMINATOR);
+	CTTrie<char, char*>  ternary_trie(ALPHABET_TERMINATOR);
 
     srand(seed);
 
     // Generate Alphabet Set - should be unique
     for(i = 0; i < ALPHABET_SIZE; )
     {
-		aui8Alphabets[i] = rand() % ALPHABET_SIZE;
+		aui8Alphabets[i] = (rand() % ALPHABET_SIZE) + 'a';
 
         for(j = 0; j < i; j++)
         {
@@ -237,11 +299,11 @@ int main(int argc, char * argv[])
     // Generate Sequences
     srand(seed);
 
-	ppTestStrList = new unsigned char *[entries];
+	ppTestStrList = new char *[entries];
 
     for(i = 0; i < entries; i++)
     {
-		ppTestStrList[i] = new unsigned char[depth + 1];
+		ppTestStrList[i] = new char[depth + 1];
 		ppTestStrList[i][depth] = ALPHABET_TERMINATOR;
         for(j = 0; j < depth; j++)
         {
@@ -253,6 +315,10 @@ int main(int argc, char * argv[])
 	{
 		ternary_trie.Insert(ppTestStrList[i], ppTestStrList[i]);
 	}
+
+	ternary_trie.InOrderTraverse(DataPrintFunc);
+	ternary_trie.PreOrderTraverse(DataPrintFunc);
+	ternary_trie.PostOrderTraverse(DataPrintFunc);
 
 	{
 		MESAURE_FUNC_EXECUTION_TIME;
